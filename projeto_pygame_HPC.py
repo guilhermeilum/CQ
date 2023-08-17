ea786d60aa60f4f74ff261eb6a80cf82c234ea1e
guilhermeilum <guilherme220044@ilum.cnpem.br>
2023-08-17 17:27:49 -0300

    projeto_HPC

--- projeto_pygame_HPC.py
+++ projeto_pygame_HPC.py
@@ -4,31 +4,37 @@ import random
 import math
 from itertools import combinations
 import numpy as np
-from PIL import Image
+import imageio
+import time
 
+
+histograma = []
+colisão = []
+cada_mol = []
 
 pygame.init()
 
-WINDOW_WIDTH, WINDOW_HEIGHT = 600, 600
+WINDOW_WIDTH, WINDOW_HEIGHT = 620, 608
 x_grafico = 500
 window = pygame.display.set_mode((WINDOW_WIDTH + x_grafico, WINDOW_HEIGHT))
 pygame.display.set_caption("Particulas")
 
 
-NUMERO_MOLECULAS = 500
+NUMERO_MOLECULAS = 1000
 
 
-VEL = 500
+VEL = 40
 RAIO = 5
 MASSA = 1
 
-VEL1 = 500
+VEL1 = 40
 RAIO1 = 5
 MASSA1 = 1
 
-DT = 0.001
-
 vel_maxima = max(VEL, VEL1)
+
+DT = 1 / (vel_maxima)
+
 
 raio_maior = max(RAIO, RAIO1)
 raio_maior *= 2
@@ -63,7 +69,7 @@ class Particle:
     def draw(self):
         pygame.draw.circle(window, self.color, (self.x, self.y), self.radius)
 
-    def colisão_parede(self):
+    def colisao_parede(self):
         x_novo = self.x + self.speed_x * DT
         y_novo = self.y + self.speed_y * DT
 
@@ -84,49 +90,69 @@ class Particle:
             self.speed_y *= -1
 
 
-def colisões_mol(mol1, mol2, list_vel):
-    distancia = math.sqrt((mol1.x - mol2.x) ** 2 + (mol1.y - mol2.y) ** 2)
+class sistema:
+    def __init__(self, particulas):
+        self.particulas = particulas
+        self.velocidades = [
+            math.sqrt(p.speed_x**2 + p.speed_y**2) for p in particulas
+        ]
+        self.lista_combinacao = list(combinations(particulas, 2))
+        print(self.lista_combinacao[0][0].x)
 
-    if distancia <= (mol1.radius + mol2.radius):
-        distancia_futura = math.sqrt(
-            ((mol1.x + mol1.speed_x * DT) - (mol2.x + mol2.speed_x * DT)) ** 2
-            + ((mol1.y + mol1.speed_y * DT) - (mol2.y + mol2.speed_y * DT)) ** 2
-        )
-        aproximando = distancia_futura < distancia
-        if aproximando:
-            delta_r = (mol1.x - mol2.x, mol1.y - mol2.y)
+    def simulacao_molecula(self, desenha):
+        for particle in self.particulas:
+            particle.move()
+            particle.colisao_parede()
+            if desenha:
+                particle.draw()
 
-            delta_v = (mol1.speed_x - mol2.speed_x, mol1.speed_y - mol2.speed_y)
+    def colisoes_mol(self):
+        for mol1_2 in self.lista_combinacao:
+            self.conta_colisao(mol1_2)
 
-            produto_interno = delta_v[0] * delta_r[0] + delta_v[1] * delta_r[1]
-
-            j = (2 * mol2.massa * mol1.massa * produto_interno) / (
-                distancia * (mol2.massa + mol1.massa)
+    def conta_colisao(self, mol1_2):
+        mol1, mol2 = mol1_2
+        distancia = math.sqrt((mol1.x - mol2.x) ** 2 + (mol1.y - mol2.y) ** 2)
+        if distancia <= (mol1.radius + mol2.radius):
+            distancia_futura = math.sqrt(
+                ((mol1.x + mol1.speed_x * DT) - (mol2.x + mol2.speed_x * DT)) ** 2
+                + ((mol1.y + mol1.speed_y * DT) - (mol2.y + mol2.speed_y * DT)) ** 2
             )
+            aproximando = distancia_futura < distancia
+            if aproximando:
+                delta_r = (mol1.x - mol2.x, mol1.y - mol2.y)
 
-            j_x = (j * delta_r[0]) / distancia
-            j_y = (j * delta_r[1]) / distancia
+                delta_v = (mol1.speed_x - mol2.speed_x, mol1.speed_y - mol2.speed_y)
 
-            vel_media1 = math.sqrt(mol1.speed_x**2 + mol1.speed_y**2)
-            vel_media2 = math.sqrt(mol2.speed_x**2 + mol2.speed_y**2)
+                produto_interno = delta_v[0] * delta_r[0] + delta_v[1] * delta_r[1]
 
-            list_vel.remove(vel_media1)
-            list_vel.remove(vel_media2)
+                j = (2 * mol2.massa * mol1.massa * produto_interno) / (
+                    distancia * (mol2.massa + mol1.massa)
+                )
 
-            mol2.speed_x += j_x / mol2.massa
-            mol2.speed_y += j_y / mol2.massa
+                j_x = (j * delta_r[0]) / distancia
+                j_y = (j * delta_r[1]) / distancia
 
-            mol1.speed_x -= j_x / mol1.massa
-            mol1.speed_y -= j_y / mol1.massa
+                vel_media1 = math.sqrt(mol1.speed_x**2 + mol1.speed_y**2)
+                vel_media2 = math.sqrt(mol2.speed_x**2 + mol2.speed_y**2)
 
-            vel_media1 = math.sqrt(mol1.speed_x**2 + mol1.speed_y**2)
-            vel_media2 = math.sqrt(mol2.speed_x**2 + mol2.speed_y**2)
+                self.velocidades.remove(vel_media1)
+                self.velocidades.remove(vel_media2)
 
-            list_vel.append(vel_media1)
-            list_vel.append(vel_media2)
+                mol2.speed_x += j_x / mol2.massa
+                mol2.speed_y += j_y / mol2.massa
+
+                mol1.speed_x -= j_x / mol1.massa
+                mol1.speed_y -= j_y / mol1.massa
+
+                vel_media1 = math.sqrt(mol1.speed_x**2 + mol1.speed_y**2)
+                vel_media2 = math.sqrt(mol2.speed_x**2 + mol2.speed_y**2)
+
+                self.velocidades.append(vel_media1)
+                self.velocidades.append(vel_media2)
 
 
-def hist(lista_vel, maxima):
+def hist(lista_vel):
     bins = 200
     hist, _ = np.histogram(lista_vel, bins=bins, range=(0, 1400))
     bin_width = x_grafico / bins
@@ -188,22 +214,23 @@ def main():
                 break
         particulas[i].x = x
         particulas[i].y = y
-    list_vel_media = [math.sqrt(p.speed_x**2 + p.speed_y**2) for p in particulas]
-    maxima_vel_media = max(list_vel_media)
+
+    meu_sistema = sistema(particulas)
 
     clock = pygame.time.Clock()
-    fps = 60
+    fps = 100
 
     frames = []
+    T = 0
+    T_max = 1
 
     while True:
-        # frame_data = pygame.surfarray.array3d(window)
-        # frame = Image.fromarray(frame_data)
-        # frames.append(frame)
+        frame_data = pygame.surfarray.array3d(window)
+        frame_data = frame_data.swapaxes(0, 1)
+        frames.append(frame_data)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                # frames[0].save('simulation.gif', save_all=True, append_images=frames[1:], loop=0, duration=100)  # Ajuste a duração conforme necessário
                 pygame.quit()
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
@@ -225,20 +252,36 @@ def main():
                     desenha = not desenha
 
         if not pausa:
+            T += DT
             window.fill(WHITE)
+            start = time.time()
             if v_hist:
-                hist(list_vel_media, maxima_vel_media)
-            
-            for mol1, mol2 in combinations(particulas, 2):
-                colisões_mol(mol1, mol2, list_vel_media)
-            for particle in particulas:
-                particle.move()
-                particle.colisão_parede()
-                if desenha:
-                    particle.draw()
+                hist(meu_sistema.velocidades)
+            end = time.time()
+            histograma.append(start - end)
+
+            start = time.time()
+            meu_sistema.colisoes_mol()
+            end = time.time()
+            colisão.append(start - end)
+
+            start = time.time()
+            meu_sistema.simulacao_molecula(desenha)
+            end = time.time()
+            cada_mol.append(start - end)
 
             pygame.display.flip()
             clock.tick(fps)
+            print(T)
+        if T >= T_max:
+            print(f"colisão demora:{sum(colisão)/len(colisão)}")
+            print(f"hist demora:{sum(histograma)/len(histograma)}")
+            print(f"mol demora:{sum(cada_mol)/len(cada_mol)}")
+
+            output_video_path = f"simulation_{DT}_{vel_maxima}.mp4"
+            imageio.mimsave(output_video_path, frames, fps=int(1 / DT))
+            pygame.quit()
+            break
 
 
 if __name__ == "__main__":
